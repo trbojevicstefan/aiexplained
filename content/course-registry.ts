60ce7cf4944c8becc59beeed712f8aac4a1bdb0f
trbojevicstefan <138625322+trbojevicstefan@users.noreply.github.com>
2026-09-02 20:36:11 +0200

    refactor: compose foundation and advanced course registries

---
+++ content/course-registry.ts
@@ -0,0 +1,4 @@
+import { courseModules as foundationCourseModules } from "./course";
+import { advancedCourseModules } from "./course-advanced";
+
+export const courseModules = [...foundationCourseModules, ...advancedCourseModules];
